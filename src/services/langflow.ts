621066c0cf7749af19a3b2e02e190ffdedc4fb6b
dikjain <dikshitmahanot2005@gmail.com>
2025-01-04 21:50:37 +0530

    try

--- src/services/langflow.ts
+++ src/services/langflow.ts
@@ -5,7 +5,7 @@ const ENDPOINT = "socialpulse_flow"; // add endpoint
 const APPLICATION_TOKEN = "AstraCS:rcljgjjLklJcKXTLqJyqSdYl:81ae80ca3f28abfe7f208501360be49a08b7467a935115189c79ca84d6635381"; // add your  application token
 
 export const runFlow = async (message: string): Promise<string> => {
-    let api_url = `/api/lf/${LANGFLOW_ID}/api/v1/run/${ENDPOINT}`;
+    const api_url = `/api/lf/${LANGFLOW_ID}/api/v1/run/${ENDPOINT}`;
 
     const payload = {
         input_value: message,
@@ -23,13 +23,16 @@ export const runFlow = async (message: string): Promise<string> => {
         return response.data.outputs[0]?.outputs[0]?.results?.message?.text || "No response received";
     } catch (error) {
         if (axios.isAxiosError(error)) {
-            api_url = `https://api.langflow.astra.datastax.com${api_url}`;
-            try {
-                const response = await axios.post(api_url, payload, { headers });
-                return response.data.outputs[0]?.outputs[0]?.results?.message?.text || "No response received";
-            } catch (error) {
+            const NewUrl = `https://api.langflow.astra.datastax.com/lf/${LANGFLOW_ID}/api/v1/run/${ENDPOINT}`
+            try{
+                await axios.post(NewUrl, payload, { headers });
+
+            }catch(error){
+
                 throw new Error(`HTTP error! status: ${error?.response?.status}`);
             }
+
+            throw new Error(`HTTP error! status: ${error?.response?.status}`);
         } else {
             throw new Error('An unexpected error occurred');
         }
