0415ce8a77e9ad161b04ae0c82248ab9645789fd
dikjain <dikshitmahanot2005@gmail.com>
2025-01-04 22:27:18 +0530

    bet

--- src/services/langflow.ts
+++ src/services/langflow.ts
@@ -2,11 +2,10 @@ import axios from 'axios';
 
 const LANGFLOW_ID = "f829f83f-e4c3-4742-89d5-9ddee4394fb0"; //add langflow id
 const ENDPOINT = "socialpulse_flow"; // add endpoint 
-const URL = "https://api.langflow.astra.datastax.com/lf/f829f83f-e4c3-4742-89d5-9ddee4394fb0/api/v1/run/socialpulse_flow"
 const APPLICATION_TOKEN = "AstraCS:rcljgjjLklJcKXTLqJyqSdYl:81ae80ca3f28abfe7f208501360be49a08b7467a935115189c79ca84d6635381"; // add your  application token
 
 export const runFlow = async (message: string): Promise<string> => {
-    const api_url = `/api/lf/${LANGFLOW_ID}/api/v1/run/socialpulse_flow`;
+    const api_url = `/api/lf/${LANGFLOW_ID}/api/v1/run/${ENDPOINT}`;
 
     const payload = {
         input_value: message,
@@ -15,7 +14,7 @@ export const runFlow = async (message: string): Promise<string> => {
     };
 
     const headers = {
-        Authorization: `Bearer ${APPLICATION_TOKEN}`,
+        Authorization: `Bearer AstraCS:rcljgjjLklJcKXTLqJyqSdYl:81ae80ca3f28abfe7f208501360be49a08b7467a935115189c79ca84d6635381`,
         "Content-Type": "application/json",
     };
 
@@ -24,12 +23,6 @@ export const runFlow = async (message: string): Promise<string> => {
         return response.data.outputs[0]?.outputs[0]?.results?.message?.text || "No response received";
     } catch (error) {
         if (axios.isAxiosError(error)) {
-            try{
-                await axios.post(URL, payload, { headers });
-            }catch{
-                console.log(error);
-
-            }
             throw new Error(`HTTP error! status: ${error?.response?.status}`);
         } else {
             throw new Error('An unexpected error occurred');
